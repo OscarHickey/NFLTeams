08392b7b9ff8178d98c19b8476b8b6268f9d1b10
OscarHickey <146850911+OscarHickey@users.noreply.github.com>
2024-02-28 11:51:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,74 @@
+var url = "https:raw.githubusercontent.com/b-mcavoy/datasets/main/Sports/NFL%20Teams.csv"
+var Conference = getColumn(url,1);
+var Divisions = getColumn(url,2);
+var Teams = getColumn(url,3);
+var City = getColumn(url,4);
+var Arena = getColumn(url,5);
+var Capacity = getColumn(url,6);
+var Coach = getColumn(url,7);
+
+
+
+//Coaches that names start with a letter
+
+
+function getCoaches(coach){
+  var matches = [];
+  
+  for(var i = 0; i < Coach.length; i++){
+    if(Coach[i].toLowerCase().includes(coach.toLowerCase())){
+      matches.push(Coach[i])
+  }
+  }
+  return matches;
+}
+
+
+//console.log(getCoaches("q"))
+
+//Purpose is to find any NFL coach using one letter or a whole name 
+
+function getCoachesteam(team){
+  var matches = [];
+  
+  for(var i = 0; i < Coach.length; i++){
+    if(Coach[i].toLowerCase().includes(team.toLowerCase())){
+      matches.push(Teams[i])
+  }
+  }
+  return matches;
+}
+//console.log(getCoachesteam("Kyle shanahan"));
+
+//Purpose is to find the team of an inputted head coach. 
+
+function getCapacity(division){
+  var total= 0;
+  for(var i = 0; i < Divisions.length; i++){
+      if(Divisions[i].toLowerCase() == division.toLowerCase()){
+          total += parseFloat(Capacity[i]); 
+      }
+  }
+  if(total == 0){
+      return -1;
+  }  
+  return total;
+}
+//console.log(getCapacity("East"));
+//console.log(getCapacity("South"));
+//console.log(getCapacity("West"));
+//console.log(getCapacity("North"));
+
+//Gives highest capacity for biggest stadium in Divisions
+
+
+
+
+
+
+
+
+
 function getColumn(url, columnNumber){
     var column = [];
     var table = [];
@@ -21,29 +92,6 @@ function getColumn(url, columnNumber){
     return column;
 }
 
-var url = "https:raw.githubusercontent.com/b-mcavoy/datasets/main/Sports/NFL%20Teams.csv"
-var Conference = getColumn(url,1);
-var Divisions = getColumn(url,2);
-var Teams = getColumn(url,3);
-var City = getColumn(url,4);
-var Arena = getColumn(url,5);
-var Capacity = getColumn(url,6);
-var Coach = getColumn(url,7);
 
-function getCapacity(division){
-  var total= 0;
-  for(var i = 0; i < Divisions.length; i++){
-      if(Divisions[i].toLowerCase() == division.toLowerCase()){
-          total += parseFloat(Capacity[i]); 
-      }
-  }
-  if(total == 0){
-      return -1;
-  }  
-  return total;
-}
-console.log(getCapacity("East"));
-console.log(getCapacity("South"));
-console.log(getCapacity("West"));
-console.log(getCapacity("North"));
-j
+
+
